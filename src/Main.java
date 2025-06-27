5f60c975293d2c37b11a35fa8e587e8827ae8627
suchi-291 <suryasuchitha@gmail.com>
2025-06-27 07:42:56 +0530

    basic commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from IntelliJ!");
+        byte age = 25;
+        int viewsCount = 222_222_222;
+
+        System.out.println(viewsCount);
+        System.out.println(age);
+
+
     }
 }
